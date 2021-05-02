239229dbe93a17447de6fa603bf14e8aeb10b64d
Drshika Asher <drshika@wirelessprv-10-192-25-73.near.illinois.edu>
2021-05-02 13:16:11 -0500

    clean up stray comments

---
+++ include/core/note.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <map>
+#include <string>
+#include <vector>
+
+#include "cinder/audio/audio.h"
+#include "cinder/audio/Source.h"
+
+namespace tanpura {
+public:
+    
+    
+    Tanpura();
+    
+private:
+}
